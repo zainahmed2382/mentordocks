4fc817c0866a294a048bb0a5e46f591e9172bc3b
zainahmed2382 <zain2407f@aptechsite.net>
2026-07-08 06:21:27 -0700

    .

---
+++ api/_prisma.ts
@@ -0,0 +1,9 @@
+import { PrismaClient } from '@prisma/client';
+import { Pool } from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+const connectionString = process.env.DATABASE_URL;
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+
+export const prisma = new PrismaClient({ adapter });
--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,10 @@
-import app from "../server.ts";
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors({ origin: process.env.CORS_ORIGIN }));
+app.use(express.json());
 
 export default app;
